a2089ba2900546345a40c3a59d01d6243e972623
labuser <labuser@ORACLE>
2024-02-23 11:57:32 +0000

    Add project files.

---
+++ Miniproject 3/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Miniproject_3.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Miniproject 3/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Miniproject_3.Models.Domain;
+
+namespace Miniproject_3.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Customer> Customers {  get; set; }
+    }
+}
